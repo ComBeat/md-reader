8b6fca76f0dbf2973a8d01dfe086b417155b4f9d
Yannik Eckert <yannik.eckert@gmx.de>
2021-05-30 00:00:44 +0200

    Implemented Quint Dice with resultin strings and images

--- app/src/main/java/com/combeat/mdreader/MainActivity.kt
+++ app/src/main/java/com/combeat/mdreader/MainActivity.kt
@@ -2,10 +2,23 @@ package com.combeat.mdreader
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton: Button = findViewById(R.id.buttonRoll)
+        rollButton.setOnClickListener {
+            val resultImageView: ImageView = findViewById(R.id.imageViewRollResult)
+            val resultTextView: TextView = findViewById(R.id.textViewRollResult)
+            val quintDice = QuintDice()
+
+            resultImageView.setImageResource(quintDice.rollForQuintImage())
+            resultTextView.setText(quintDice.rollForQuintText())
+        }
     }
 }
---
+++ app/src/main/java/com/combeat/mdreader/QuintDice.kt
@@ -0,0 +1,45 @@
+package com.combeat.mdreader
+
+/**
+ * Class which rolls one of the quints
+ */
+class QuintDice() {
+	private val sides: Int = 6
+	private val rollNumber: Int = (1..sides).random()
+
+	/**
+	 * Returns the ID of the result string
+	 */
+	fun rollForQuintText(): Int {
+		var resultTextId: Int = 0
+
+		when (rollNumber) {
+			1 -> resultTextId = R.string.result_roll_ichika
+			2 -> resultTextId = R.string.result_roll_nino
+			3 -> resultTextId = R.string.result_roll_miku
+			4 -> resultTextId = R.string.result_roll_yotsuba
+			5 -> resultTextId = R.string.result_roll_itsuki
+			6 -> resultTextId = R.string.result_roll_nakanos
+		}
+
+		return resultTextId
+	}
+
+	/**
+	 * Returns the ID of the result image
+	 */
+	fun rollForQuintImage(): Int {
+		var resultDrawableId: Int = 0
+
+		when (rollNumber) {
+			1 -> resultDrawableId = R.drawable.image_ichika
+			2 -> resultDrawableId = R.drawable.image_nino
+			3 -> resultDrawableId = R.drawable.image_miku
+			4 -> resultDrawableId = R.drawable.image_yotsuba
+			5 -> resultDrawableId = R.drawable.image_itsuki
+			6 -> resultDrawableId = R.drawable.image_quints_1
+		}
+
+		return resultDrawableId
+	}
+}
